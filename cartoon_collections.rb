ae221036d4a09cfaa251e19c2ea05f3b14c6453e
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 22:23:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_list)
+  dwarves_list.map{ |i| "${i+1}. ${dwarves_list[i]}"}
 end
 
 def summon_captain_planet# code an argument here
