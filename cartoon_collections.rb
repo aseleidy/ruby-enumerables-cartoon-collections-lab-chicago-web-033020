4fcc98bca9d3a5ed1cca96257e2e1ab6864c0b66
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 22:48:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 
 def roll_call_dwarves(dwarves)
   
-  dwarves.map{|i, n| p "#{n}. #{i}"}.join("\n")
+  dwarves.map{|i, n| p "#{n}. #{i}"\n}.join(" ")
 end
 
 def summon_captain_planet# code an argument here
