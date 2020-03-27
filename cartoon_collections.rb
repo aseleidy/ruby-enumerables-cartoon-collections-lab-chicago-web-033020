815e9e2fe09e8a1da95493afb550b52a2cfa5cc8
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-27 00:51:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.select {|potential_cheese| cheese_types.include? p potential_cheese}
+  array.select {|potential_cheese| cheese_types.include? p potential_cheese.sum("")}
   
 end
   
